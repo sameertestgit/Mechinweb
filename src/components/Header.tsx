7b50871383f0e0c1494ab3ad013e0c6dea277115
sameertestgit <mca24.sameerbhati@poddarinstitute.org>
2025-08-24 22:42:28 +0530

    Remove all Search references from Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,16 +87,6 @@ export default function Header() {
             >
               Contact
             </button>
-            
-            {/* Search Bar */}
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-              <input
-                type="text"
-                placeholder="Search services..."
-                className="pl-10 pr-4 py-2 bg-white/90 backdrop-blur-sm border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-transparent text-sm"
-              />
-            </div>
           </nav>
 
           {/* Auth Buttons */}
